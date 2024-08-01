a67213cea47de2bf4f3d357913c55bfa0b01daea
rastafasha <mercadocreativo@gmail.com>
2024-08-01 19:36:49 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { key } from '../key';
+import { GoogleGeminiProService } from './services/google-gemini-pro.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+  title = 'Angular Google Gemini IA';
+  result = '';
+  prompt = '';
+
+  constructor(private googleGeminiPro: GoogleGeminiProService) {
+    this.googleGeminiPro.initialize(key);
+  }
+
+  async generate() {
+    this.result = await this.googleGeminiPro.generateText(this.prompt);
+  }
+
+  limpiar(){
+    location.reload();
+  }
+
+}
---
+++ src/key.ts
@@ -0,0 +1,2 @@
+// get key here https://makersuite.google.com/app/prompts/new_freeform
+export const key = 'AIzaSyDDg9ncR9HXTV-LTrW0YZSFt19mywAlTlg';
